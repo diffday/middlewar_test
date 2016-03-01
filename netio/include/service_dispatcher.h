e937fc1ae13ac64ebc08de0acefcd01d7514df61
highsky2002 <highsky2002@gmail.com>
2016-03-01 20:06:34 +0800

    增加dispatcher接口定义

--- netio/include/service_dispatcher.h
+++ netio/include/service_dispatcher.h
@@ -7,9 +7,18 @@
 
 #ifndef NETIO_INCLUDE_SERVICE_DISPATCHER_H_
 #define NETIO_INCLUDE_SERVICE_DISPATCHER_H_
+#include <vector>
+#include "intf_service.h"
+#include "cmd_obj.h"
+using namespace std;
+class CServiceDispatcher {
+private:
+	vector<IService*> vecStatefulSvcQueue;
+	vector<IService*> vecStatelessSvcQueue;
+	int m_iCmd;
 
-
-
-
+	//处理结果是直接写入队列的，所以可以设计为没有通用传出参数
+	int Dispatch(CCmd oCmd);//调用IService对象的Execute函数
+};
 
 #endif /* NETIO_INCLUDE_SERVICE_DISPATCHER_H_ */
